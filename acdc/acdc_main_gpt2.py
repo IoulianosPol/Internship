2d9dd189bb3dd6bbe834a2693c2802b104e74e5d
it2022091@hua.gr <it2022091@hua.gr>
2026-06-29 09:59:52 +0300

    Docstring for gpt2

--- acdc/acdc_main_gpt2.py
+++ acdc/acdc_main_gpt2.py
@@ -263,8 +263,8 @@ def parse_args():
     parser.add_argument("--abs-value-threshold", action='store_true')
 
     args = parser.parse_args(
-        [line.strip() for line in r"""--task=induction\
---threshold=0.5623\
+        [line.strip() for line in r"""--task=docstring\
+--threshold=0.0095\
 --indices-mode=reverse\
 --first-cache-cpu=False\
 --second-cache-cpu=False\
